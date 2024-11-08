25229e2947686ce18a5bca3055b3180891d9f399
RN730 <134727660+RN730@users.noreply.github.com>
2024-11-08 14:47:07 -0500

    Update HeroSection.js
    Minor change

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ function HeroSection() {
   return (
     <div className="bg-success text-white text-center py-5">
       <Container>
-        <h1 className="display-4">Find the Right Internship for You</h1>
+        <h1 className="display-4">Find the Right Skills for You</h1>
         <p className="lead">
           Upload your resume to see if your skills match with the latest internship requirements!
         </p>
