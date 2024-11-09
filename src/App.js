37b9e62ea247b80a834e07458124cbb8191322b2
RN730 <134727660+RN730@users.noreply.github.com>
2024-11-08 21:41:43 -0500

    Update App.js
    an update to the order and visial aspects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
+import JobFieldSelection from './components/JobFieldSelection';
 import ResumeUpload from './components/ResumeUpload';
 import SkillsComparisonInfo from './components/SkillsComparisonInfo';
 import Footer from './components/Footer';
 import EducationalResources from './components/EducationalResources';
+import AboutUs from './components/AboutUs';
+import TitleSection from './components/TitleSection';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <TitleSection /> 
       <HeroSection />
+      <AboutUs />
+     
+      <JobFieldSelection />
       <ResumeUpload />
-      <SkillsComparisonInfo />
       <EducationalResources />
       <Footer />
     </div>
