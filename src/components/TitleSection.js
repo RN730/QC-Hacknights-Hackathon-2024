7aad7def83b0763265e51d801d45d3271d05a352
RN730 <134727660+RN730@users.noreply.github.com>
2024-11-08 20:34:23 -0500

    circle animation isntead
    tried a circular animation

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import './TitleSection.css'; 
+import './TitleSection.css';
 
-function TitleSection() {
+const TitleSection = () => {
   return (
-    <div className="title-section">
-      <div className="dodecahedron-container">
-        <div className="face"></div>
-        <div className="face"></div>
-        <div className="face"></div>
-        <div className="face"></div>
-        <div className="face"></div>
-        <div className="face"></div>
-        <div className="face"></div>
+    <section className="title-section">
+      <div className="hexagon-container">
+        <div className="hexagon">
+          <div className="hexagon-face"></div>
+          <div className="hexagon-face"></div>
+          <div className="hexagon-face"></div>
+          <div className="hexagon-face"></div>
+          <div className="hexagon-face"></div>
+          <div className="hexagon-face"></div>
+        </div>
       </div>
-      <Container className="text-center py-5">
-        <h1 className="app-title">Skills Match</h1>
-        <p className="app-motto">Personalized Skills just for you</p>
-      </Container>
-    </div>
+      <h1 className="app-title">Skills Match</h1>
+      <p className="app-motto">Personalized Skills Just For You</p>
+    </section>
   );
-}
+};
 
 export default TitleSection;
