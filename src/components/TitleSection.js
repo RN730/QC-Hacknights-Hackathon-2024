305e1dfd9fa0c83466ab5701ce153fee24a9ec5b
RN730 <134727660+RN730@users.noreply.github.com>
2024-11-08 20:07:04 -0500

    3d animation
    added a 3d animation effect, plan to improve it more

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -5,6 +5,15 @@ import './TitleSection.css';
 function TitleSection() {
   return (
     <div className="title-section">
+      <div className="dodecahedron-container">
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+        <div className="face"></div>
+      </div>
       <Container className="text-center py-5">
         <h1 className="app-title">Skills Match</h1>
         <p className="app-motto">Personalized Skills just for you</p>
