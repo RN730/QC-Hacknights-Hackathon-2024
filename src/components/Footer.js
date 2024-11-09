a9b32b685652568791f5edc584607e852518e749
RN730 <134727660+RN730@users.noreply.github.com>
2024-11-08 19:45:11 -0500

    Update Footer.js
    our chosen name is skills match

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="bg-dark text-white py-3">
       <Container className="text-center">
-        <p>&copy; 2024 Internship Matcher. All rights reserved.</p>
+        <p>&copy; 2024 Skills Match. All rights reserved.</p>
         <a href="#terms" className="text-light mx-2">Terms</a>
         <a href="#privacy" className="text-light mx-2">Privacy Policy</a>
       </Container>
